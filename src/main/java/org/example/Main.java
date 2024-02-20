73c5b9e737f0a57627ff61ed7fbd1dabb68a8628
lenblazy <lenblazy@gmail.com>
2024-02-20 11:25:42 +0300

    String builder recap

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.example.string.MyStrBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        MyStrBuilder strBuilder = new MyStrBuilder();
+        strBuilder.buildStr();
+
+    }
+}
---
+++ src/main/java/org/example/string/MyStrBuilder.java
@@ -0,0 +1,16 @@
+package org.example.string;
+
+public class MyStrBuilder {
+
+    public void buildStr(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello")
+                .append("world")
+                .append(" 2024");
+        System.out.println(sb);
+
+        //////////////////////////////// Formatting ////////////////////////////////
+
+    }
+
+}
