9db7bd76e798e12e9db450dde1c69240d23b426b
lenblazy <lenblazy@gmail.com>
2024-02-23 12:28:37 +0300

    Working on collections

---
+++ collections/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        for (int j : arr) {
+            System.out.println(j);
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        String[] names = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
+
+        System.out.println(Arrays.toString(names));
+
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Yow");
+        System.out.println(list);
+
+    }
+}
