f81d7106447bb622b64e01719505c41ab4bbe52c
lenblazy <lenblazy@gmail.com>
2024-02-25 11:48:48 +0300

    Finish find last position array

---
+++ binarysearch/src/main/java/org/example/FirstLastPositionArray.java
@@ -0,0 +1,70 @@
+package org.example;
+
+/*
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+nums is a non-decreasing array.
+-109 <= target <= 109
+ */
+public class FirstLastPositionArray {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int startIdx = search(nums, target, true);
+        int endIdx = search(nums, target, false);
+        return new int[]{startIdx, endIdx};
+    }
+
+    int search(int[] arr, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+}
