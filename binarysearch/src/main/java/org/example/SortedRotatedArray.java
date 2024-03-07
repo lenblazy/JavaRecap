87f1a39ba857c643d6ad5ba180aa76c5b11f01ab
lenblazy <lenblazy@gmail.com>
2024-03-07 09:38:44 +0300

    Rotated array in duplicated

--- binarysearch/src/main/java/org/example/SortedRotatedArray.java
+++ binarysearch/src/main/java/org/example/SortedRotatedArray.java
@@ -101,5 +101,47 @@ public class SortedRotatedArray {
 
     }
 
+    static int findPivotDuplicate(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip dupes
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+
+                start++;
+
+                if (arr[end] > arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+
+            }
+            //left side sorted, so pivot should be in the right
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+
+        }
+
+        return -1;
+
+    }
+
 
 }
