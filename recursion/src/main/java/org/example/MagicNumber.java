fb7384a746e57da126be82081f37227c3878c97d
lenblazy <lenblazy@gmail.com>
2024-03-29 17:41:41 +0300

    Add magic number solution

---
+++ recursion/src/main/java/org/example/MagicNumber.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/**
+ *
+ */
+public class MagicNumber {
+
+    public static void main(String[] args) {
+        int n = 1;
+
+        int ans = 0;
+        int base = 5;
+
+        while (n > 0) {
+            int last = n & 1;
+            n = n >> 1;
+            ans += last * base;
+            base = base * 5;
+        }
+
+        System.out.println(ans);
+    }
+
+
+
+}
--- recursion/src/main/java/org/example/Operators.java
+++ recursion/src/main/java/org/example/Operators.java
@@ -3,11 +3,13 @@ package org.example;
 public class Operators {
 
     public static void main(String[] args) {
-//        byte n = 61;
+        byte n = 127;
 //        System.out.println(isOdd(n));
 
-        int[] arr = {2, 3, 3, 4, 2, 6, 4};
-        System.out.println(findUnique(arr));
+//        int[] arr = {2, 3, 3, 4, 2, 6, 4};
+//        System.out.println(findUnique(arr));
+//        int[] arr = {2, 2, 3, 7, 7, 8, 7, 8, 8};
+//        System.out.println(findUniqueAppearing3Times(arr));
     }
 
     private static boolean isOdd(byte n) {
@@ -17,8 +19,18 @@ public class Operators {
     private static int findUnique(int[] arr) {
         int unique = 0;
 
-        for(int n: arr){
+        for (int n : arr) {
             unique ^= n;
+        }
+
+        return unique;
+    }
+
+    private static int findUniqueAppearing3Times(int[] arr) {
+        int unique = 0;
+
+        for (int n : arr) {
+            unique &= n;
         }
 
         return unique;
