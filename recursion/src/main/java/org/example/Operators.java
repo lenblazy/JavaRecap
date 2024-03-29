be17d461cff334d484ed885145a0b91a6a51bafb
lenblazy <lenblazy@gmail.com>
2024-03-29 10:09:08 +0300

    Finc unique number in O(n) time complexity

--- recursion/src/main/java/org/example/Operators.java
+++ recursion/src/main/java/org/example/Operators.java
@@ -3,13 +3,25 @@ package org.example;
 public class Operators {
 
     public static void main(String[] args) {
-        int n = 60;
-        System.out.println(isOdd(n));
+//        byte n = 61;
+//        System.out.println(isOdd(n));
+
+        int[] arr = {2, 3, 3, 4, 2, 6, 4};
+        System.out.println(findUnique(arr));
     }
 
-    private static boolean isOdd(int n) {
+    private static boolean isOdd(byte n) {
         return (n & 1) == 1;
     }
 
+    private static int findUnique(int[] arr) {
+        int unique = 0;
+
+        for(int n: arr){
+            unique ^= n;
+        }
+
+        return unique;
+    }
 
 }
