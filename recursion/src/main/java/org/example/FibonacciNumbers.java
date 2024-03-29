a2b4d1aa42b72371b83022bd93405535c5db3a55
lenblazy <lenblazy@gmail.com>
2024-03-29 09:52:00 +0300

    Use and operator to check if number is odd

--- recursion/src/main/java/org/example/FibonacciNumbers.java
+++ recursion/src/main/java/org/example/FibonacciNumbers.java
@@ -3,7 +3,8 @@ package org.example;
 public class FibonacciNumbers {
 
     public static void main(String[] args) {
-        int ans = fibo(4);
+//        int ans = fibo(4);
+        int ans = fiboFormula(50);
         System.out.println(ans);
     }
 
@@ -15,6 +16,10 @@ public class FibonacciNumbers {
         }
 
         return fibo(n - 1) + fibo(n - 2);
+    }
+
+    static int fiboFormula(int n) {
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) / Math.sqrt(5));
     }
 
 }
---
+++ recursion/src/main/java/org/example/Operators.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        int n = 60;
+        System.out.println(isOdd(n));
+    }
+
+    private static boolean isOdd(int n) {
+        return (n & 1) == 1;
+    }
+
+
+}
