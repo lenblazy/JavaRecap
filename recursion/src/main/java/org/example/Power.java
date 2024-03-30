c1882aea06beba217f4f6a9e2fee7bf70a6e2a39
lenblazy <lenblazy@gmail.com>
2024-03-30 09:16:18 +0300

    Find power in n log n

---
+++ recursion/src/main/java/org/example/Power.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Power {
+
+    public static void main(String[] args) { //O(log b) log base times
+        int base = 3;
+        int power = 6;
+
+        int ans = 1;
+
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                ans *= base;
+            }
+
+            base *= base;
+            power = power >> 1;
+        }
+        System.out.println(ans);
+    }
+
+}
