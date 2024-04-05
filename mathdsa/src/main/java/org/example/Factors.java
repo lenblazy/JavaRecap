09880cbd1ebd2e788549683de7f8091a1998ec54
lenblazy <lenblazy@gmail.com>
2024-04-05 09:19:23 +0300

    Improved factors program

--- mathdsa/src/main/java/org/example/Factors.java
+++ mathdsa/src/main/java/org/example/Factors.java
@@ -1,16 +1,51 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Factors {
 
     public static void main(String[] args) {
-        factors1(20);
+        factors3(20);
     }
 
+    //    TC= O(n)
     static void factors1(int n) {
         for (int i = 1; i <= n; i++) {
-            if (n % i == 0){
-                System.out.println(i + " ");
+            if (n % i == 0) {
+                System.out.print(i + " ");
             }
+        }
+    }
+
+//    TC=O(sqrt(n)) SC=O(1)
+    static void factors2(int n) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " " + n / i + " ");
+                }
+            }
+        }
+    }
+
+    //    TC=O(sqrt(n)) SC=O(1)
+    static void factors3(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " ");
+                    list.add(n / i );
+                }
+            }
+        }
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i)+ " ");
         }
     }
 
