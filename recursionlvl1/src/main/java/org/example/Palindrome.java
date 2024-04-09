0249e4cb014b1da3d13c21ef5aed262ba9f7dc41
lenblazy <lenblazy@gmail.com>
2024-04-09 08:32:43 +0300

    Palindrome of digits

---
+++ recursionlvl1/src/main/java/org/example/Palindrome.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Palindrome {
+
+
+    private static int rev(int n) {
+        // Sometimes you might need some additional variable in the argument,
+        // in that case make another function
+        int digits = (int)(Math.log10(n)) + 1;
+        return helper(n, digits);
+    }
+
+    private static int helper(int n, int digits) {
+        if (n % 10 == n){
+            return n;
+        }
+
+        int rem = n % 10;
+        return rem * (int)(Math.pow(10, digits - 1)) + helper(n / 10, digits - 1);
+    }
+
+    static boolean isPalindrome(int n) {
+        return n == rev(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1234321));
+    }
+
+
+}
