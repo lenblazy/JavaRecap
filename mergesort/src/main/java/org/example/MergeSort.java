6196e5badd5e8901968670fc045ce326abf6cf21
lenblazy <lenblazy@gmail.com>
2024-04-18 08:56:41 +0300

    merge sort in place

--- mergesort/src/main/java/org/example/MergeSort.java
+++ mergesort/src/main/java/org/example/MergeSort.java
@@ -8,12 +8,12 @@ public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
         System.out.println("the arr before was " + Arrays.toString(arr));
-        arr = mergeSort(arr);
+        mergeSortInPlace(arr, 0, arr.length);
         System.out.println("the arr is now " + Arrays.toString(arr));
     }
 
     private static int[] mergeSort(int[] arr) {
-        if (arr.length == 1){
+        if (arr.length == 1) {
             return arr;
         }
 
@@ -50,6 +50,47 @@ public class MergeSort {
         }
 
         return mix;
+    }
+
+    private static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = s + (e - s) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+         mergeInPlace(arr, s, mid, e);
+    }
+
+    private static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k++] = arr[i++];
+            } else {
+                mix[k++] = arr[j++];
+            }
+        }
+
+        while (i < m) {
+            mix[k++] = arr[i++];
+        }
+
+        while (j < e) {
+            mix[k++] = arr[j++];
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
     }
 
 }
