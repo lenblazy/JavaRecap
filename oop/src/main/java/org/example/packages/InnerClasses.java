6702fa8b51c0e657ef29088ecc2dcdbec7f51898
lenblazy <lenblazy@gmail.com>
2024-05-16 09:14:46 +0300

    Use inner classes

---
+++ oop/src/main/java/org/example/packages/InnerClasses.java
@@ -0,0 +1,24 @@
+package org.example.packages;
+
+
+
+public class InnerClasses {
+
+    static class Test {
+        String name;
+
+        public Test(String name) {
+            this.name = name;
+        }
+
+    }
+
+    public static void main(String[] args) {
+//        Test a = new Test("Test");
+    }
+
+}
+
+//static class A {
+//
+//}
