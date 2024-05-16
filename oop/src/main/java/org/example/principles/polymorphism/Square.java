e47064044f0d09bae3424bf30505cebe25143041
lenblazy <lenblazy@gmail.com>
2024-05-16 10:02:42 +0300

    Polymorphism

---
+++ oop/src/main/java/org/example/principles/polymorphism/Square.java
@@ -0,0 +1,9 @@
+package org.example.principles.polymorphism;
+
+public class Square extends Shapes {
+
+    void area() {
+        System.out.println("I am in square");
+    }
+
+}
---
+++ oop/src/main/java/org/example/principles/polymorphism/Triangle.java
@@ -0,0 +1,8 @@
+package org.example.principles.polymorphism;
+
+public class Triangle {
+
+    void area() {
+        System.out.println("I am in triangle");
+    }
+}
