921c331b8d4911c2d2664518548bbe30a6af814f
Lennox Mwabonje <lenblazy@gmail.com>
2025-02-22 11:20:47 +0300

    Cysclic sort

--- sorting/src/main/java/org/example/cyclic/CyclicSort.java
+++ sorting/src/main/java/org/example/cyclic/CyclicSort.java
@@ -7,13 +7,14 @@ public class CyclicSort {
     public static void main(String[] args) {
         int[] arr = {3, 4, 2, 5, 1};
         cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
     static void cyclicSort(int[] arr) {
         int i = 0;
         while (i < arr.length) {
             int correctIdx = arr[i] - 1;
-            if (arr[i] != arr[correctIdx]) {
+            if (i != correctIdx) {
                 swap(arr, i, correctIdx);
             } else {
                 i++;
